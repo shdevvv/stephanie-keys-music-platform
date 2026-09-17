48d7e555f1fab37de08222381f173cb825476236
shdevvv <shdevvv@users.noreply.github.com>
2026-09-18 00:28:08 +0700

    Increase white overlay opacity to 30% for background photo

--- frontend/src/homepage.tsx
+++ frontend/src/homepage.tsx
@@ -398,8 +398,8 @@ function Homepage() {
                   src="/royal-grand-hall-bg.jpg"
                 />
               </div>
-              {/* Soft 18% White Overlay Layer */}
-              <div className="absolute inset-0 bg-white/18 z-0 pointer-events-none" />
+              {/* Soft 30% White Overlay Layer */}
+              <div className="absolute inset-0 bg-white/30 z-0 pointer-events-none" />
 
               {/* CSS Keyframes for falling glitters */}
               <style dangerouslySetInnerHTML={{
